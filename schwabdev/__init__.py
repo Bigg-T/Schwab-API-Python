d91cb66b5b169ca135d92033f6250fae40ca45cf
Tyler Bowers <tylerebowers@gmail.com>
2026-08-07 16:11:04 -0400

    Change submodule name

--- schwabdev/__init__.py
+++ schwabdev/__init__.py
@@ -2,7 +2,7 @@ from .client import Client, ClientAsync
 from .stream import Stream, StreamAsync
 from .translate import stream_fields
 try:
-    from schwabdev_trader import Trader
+    from schwabdev_context import Context, Costs
 except ImportError:
     pass
 __version__ = "4.0.0"
